965ca8f140653a56d828f414facc5494a7f7696a
Robert Biro <darthjdg@gmail.com>
2016-08-31 23:40:50 +0100

    Add copyright header to test file

--- test/test.js
+++ test/test.js
@@ -1,3 +1,25 @@
+/**
+ * Mangler.js - JavaScript object processing library
+ * Copyright (C) 2014-2016
+ *
+ * Project: [http://codebin.co.uk/projects/mangler-js/]
+ * GitHub:  [https://github.com/DarthJDG/Mangler.js]
+ *
+ * See AUTHORS file for the list of contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 (function(global, undefined) {
 
 	QUnit.module('Global');
